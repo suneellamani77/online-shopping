762482721b452dc88be757f2c8d2f35e49eb4a2a
Suneel <suneellamani77@gmail.com>
2018-03-13 11:38:14 +0530

    My project setup successfully completed.

---
+++ shoppingbackend/src/main/java/net/sj/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.sj.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
